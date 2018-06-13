ae9435bab19fd180205e420bf8b4c6692b0c8bbe
Violetta Kolunova <vetja93@inbox.lv>
2018-06-13 22:15:38 +0300

    Napisala "ugadajka"

---
+++ src/Ugadajka.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ugadajka {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int n = r.nextInt(100);
+        while (true) {
+            System.out.print("Is it ");
+            System.out.print(n);
+            System.out.println("?");
+            Scanner scanner = new Scanner(System.in);
+            String enteredLine = scanner.nextLine();
+            if (enteredLine.equals("=")) {
+                System.out.println("I win");
+                break;
+            } else {
+                if (enteredLine.equals(">")) {
+                    n = r.nextInt(100 - n) + n;
+                }
+                if (enteredLine.equals("<")) {
+                    n = r.nextInt(n);
+                }
+            }
+        }
+    }
+}
--- src/Zagadajka.java
+++ src/Zagadajka.java
@@ -1,24 +1,21 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class Violetta {
-    public static void main(String[] args){
+public class Zagadajka {
+    public static void main(String[] args) {
         System.out.println("Guess a number");
-        Random r=new Random();
-        int n=r.nextInt(10);
+        Random r = new Random();
+        int n = r.nextInt(100);
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             int someInt = Integer.valueOf(scanner.nextLine());
-            if(n==someInt){
+            if (n == someInt) {
                 System.out.println("Yes");
                 break;
-            } else{
-                if(n<someInt){
+            } else {
+                if (n < someInt) {
                     System.out.println("Your number is bigger");
-                }
-                else{
+                } else {
                     System.out.println("Your number is smaller");
                 }
                 System.out.println("No, try again");
