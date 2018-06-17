a575743cc8b9dfbe90077fcf9f9525498d3581c0
ViolettaKolunova <vetja93@inbox.lv>
2018-06-17 14:21:41 +0300

    Napisala d/z Boljshee iz trjoh

---
+++ src/BoljsheeIzTrjoh.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BoljsheeIzTrjoh {
+    public static void main(String[] args) {
+        System.out.println("Enter number A");
+        Scanner scanner = new Scanner(System.in);
+        int someIntA = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter number B");
+        int someIntB = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter number C");
+        int someIntC= Integer.valueOf(scanner.nextLine());
+        if ((someIntA == someIntB) && (someIntB == someIntC)) {
+            System.out.println("A=B=C");
+            return;
+        }
+        System.out.println("Biggest number is ");
+        if ((someIntA > someIntB) && (someIntA > someIntC)) {
+            System.out.println(someIntA);
+        }
+        if ((someIntA < someIntB) && (someIntB < someIntC)) {
+            System.out.println(someIntC);
+        }
+        if ((someIntA < someIntB) && (someIntB > someIntC)) {
+            System.out.println(someIntB);
+        }
+    }
+}
